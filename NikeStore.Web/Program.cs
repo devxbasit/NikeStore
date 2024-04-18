acf72659e69c2188cc4412ec2e10d73f95a0d5e5
Basit <devxbasit@gmail.com>
2024-04-18 22:39:41 +0530

    Added Auth TokenProviderService

--- NikeStore.Web/Program.cs
+++ NikeStore.Web/Program.cs
@@ -1,3 +1,5 @@
+using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using NikeStore.Web.Service;
 using NikeStore.Web.Service.IService;
 using NikeStore.Web.Utility;
@@ -10,8 +12,20 @@ builder.Services.AddHttpContextAccessor();
 builder.Services.AddHttpClient();
 builder.Services.AddScoped<IBaseService, BaseService>();
 builder.Services.AddScoped<ICouponService, CouponService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+builder.Services.AddScoped<ITokenProviderService, ITokenProviderService>();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.ExpireTimeSpan = TimeSpan.FromHours(10);
+        options.LoginPath = "/Auth/Login";
+        options.LogoutPath = "/Auth/AccessDenied";
+    });
 
 SD.CouponApiBase = builder.Configuration["ServiceUrls:CouponApi"];
+SD.AuthApiBase = builder.Configuration["ServiceUrls:AuthApi"];
 
 
 var app = builder.Build();
--- NikeStore.Web/Service/TokenProviderService.cs
+++ NikeStore.Web/Service/TokenProviderService.cs
@@ -1,21 +1,29 @@
 using NikeStore.Web.Service.IService;
+using NikeStore.Web.Utility;
 
 namespace NikeStore.Web.Service;
-
-public class TokenProviderService : ITokenProviderService
+public class TokenProvider : ITokenProviderService
 {
-    public void SetToken(string token)
+    private readonly IHttpContextAccessor _contextAccessor;
+    public TokenProvider(IHttpContextAccessor contextAccessor)
     {
-        throw new NotImplementedException();
-    }
-
-    public string? GetToken()
-    {
-        throw new NotImplementedException();
+        _contextAccessor = contextAccessor;
     }
 
     public void ClearToken()
     {
-        throw new NotImplementedException();
+        _contextAccessor.HttpContext?.Response.Cookies.Delete(SD.TokenCookie);
+    }
+
+    public string? GetToken()
+    {
+        string? token = null;
+        bool? hasToken = _contextAccessor.HttpContext?.Request.Cookies.TryGetValue(SD.TokenCookie, out token);
+        return hasToken is true ? token : null;
+    }
+
+    public void SetToken(string token)
+    {
+        _contextAccessor.HttpContext?.Response.Cookies.Append(SD.TokenCookie, token);
     }
 }
