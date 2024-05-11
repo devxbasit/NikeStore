3d2d684900f05dafbc5366ac36ad0a32187e35b4
Basit <devxbasit@gmail.com>
2024-05-11 19:03:03 +0530

    Fixed: CouponApi Web

--- NikeStore.Web/Controllers/AuthController.cs
+++ NikeStore.Web/Controllers/AuthController.cs
@@ -114,15 +114,19 @@ public class AuthController : Controller
         var jwt = handler.ReadJwtToken(model.Token);
 
         var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+        
         identity.AddClaim(new Claim(JwtRegisteredClaimNames.Email,
             jwt.Claims.FirstOrDefault(u => u.Type == JwtRegisteredClaimNames.Email).Value));
+        
         identity.AddClaim(new Claim(JwtRegisteredClaimNames.Sub,
             jwt.Claims.FirstOrDefault(u => u.Type == JwtRegisteredClaimNames.Sub).Value));
+        
         identity.AddClaim(new Claim(JwtRegisteredClaimNames.Name,
             jwt.Claims.FirstOrDefault(u => u.Type == JwtRegisteredClaimNames.Name).Value));
 
         identity.AddClaim(new Claim(ClaimTypes.Name,
             jwt.Claims.FirstOrDefault(u => u.Type == JwtRegisteredClaimNames.Email).Value));
+
         identity.AddClaim(new Claim(ClaimTypes.Role,
             jwt.Claims.FirstOrDefault(u => u.Type == "role").Value));
 
