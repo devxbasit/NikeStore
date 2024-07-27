61661f8160d4fad47cb1598bbe457b1d6e3ce6ee
Basit <devxbasit@gmail.com>
2024-07-27 20:09:43 +0530

    Fixed iframe onload callback executed twice

--- AngularDashboardApp/nikestore-dashboard/src/app/shared/components/iframe-wrapper/iframe-wrapper.component.ts
+++ AngularDashboardApp/nikestore-dashboard/src/app/shared/components/iframe-wrapper/iframe-wrapper.component.ts
@@ -1,18 +1,29 @@
-import { Component, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-iframe-wrapper',
   templateUrl: './iframe-wrapper.component.html',
   styleUrls: ['./iframe-wrapper.component.css'],
 })
-export class IframeWrapperComponent {
+export class IframeWrapperComponent implements AfterViewInit {
   @Input({ required: true }) src = '';
   @Input({ required: true }) title = '';
   @Input({ required: true }) height = '';
+  @ViewChild('iframe') iFrame!: ElementRef<HTMLIFrameElement>;
 
   isLoading = true;
 
-  onLoadEnd() {
-    this.isLoading = false;
+  ngAfterViewInit(): void {
+    if (this.iFrame) {
+      this.iFrame.nativeElement.onload = () => {
+        this.isLoading = false;
+      };
+    }
   }
 }
